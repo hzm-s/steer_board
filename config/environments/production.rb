dce1aa6776a554937c786005d9a79a60d797c264
hzms <h4zime@gmail.com>
2023-09-02 11:09:44 +0900

    rename app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "showboard_production"
+  # config.active_job.queue_name_prefix = "steer_board_production"
 
   config.action_mailer.perform_caching = false
 
