20b82d2a4ebe7ded886db5899698baf91261a8bd
hzms <h4zime@gmail.com>
2023-09-04 00:29:44 +0900

    add font-awesome kit

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,5 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "font-awesome", to: "https://kit.fontawesome.com/21f6899a03.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
