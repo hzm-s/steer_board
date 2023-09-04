5ce4323be7105410539a09c77aa5f9197dad3a0a
hzms <h4zime@gmail.com>
2023-09-04 19:56:04 +0900

    ./bin/importmap pin stimulus-dropdown --download

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,7 +2,10 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "font-awesome", to: "https://kit.fontawesome.com/21f6899a03.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "stimulus-dropdown" # @2.1.0
+pin "hotkeys-js" # @3.12.0
+pin "stimulus-use" # @0.51.3
