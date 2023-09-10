b54abd272bfb513dbc5656fc35b2fdba160b8a4e
hzms <h4zime@gmail.com>
2023-09-10 12:29:47 +0900

    refactor icon component constructor args

--- app/components/icon_component.rb
+++ app/components/icon_component.rb
@@ -1,13 +1,13 @@
 class IconComponent < ViewComponent::Base
   def initialize(**attrs)
     @name = attrs[:name]
-    @size = attrs[:size] || 1.5
     @style = attrs[:style] || :solid
-    @extra_class = attrs[:class] || ''
+    @size = attrs[:size] || 1.5
+    @extra_class = attrs[:class] || []
   end
 
   def icon_class
-    "fa-#{@style.to_s} fa-#{@name.to_s}"
+    ["fa-#{@style}", "fa-#{@name}"]
   end
 
   private
