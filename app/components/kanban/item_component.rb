712bc77a2015a56dc8cd7fabe419c706f423f80d
hzms <h4zime@gmail.com>
2023-09-17 15:24:35 +0900

    fake item id

--- app/components/kanban/item_component.rb
+++ app/components/kanban/item_component.rb
@@ -5,6 +5,10 @@ class Kanban::ItemComponent < ViewComponent::Base
     @item = item
   end
 
+  def item_id
+    (100..3000).to_a.sample
+  end
+
   def contributors
     @__contributors ||= (0..3).to_a.sample.times.map { Member.random }
   end
