ab0a10e905f5f27dbf691bfafc0927acc74bb98e
hzms <h4zime@gmail.com>
2023-09-18 10:51:44 +0900

    item size element size option

--- app/components/item_size_component.rb
+++ app/components/item_size_component.rb
@@ -1,5 +1,7 @@
 class ItemSizeComponent < ViewComponent::Base
-  def initialize(value:)
-    @value = value
+  def initialize(**args)
+    @size = args[:size]
+    @element_size = args[:element_size] || :sm
+    @extra_class = args[:class]
   end
 end
