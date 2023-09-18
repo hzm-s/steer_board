38d93de9b742a3c8f54e7e8b8c7e097fb623bfaf
hzms <h4zime@gmail.com>
2023-09-18 11:47:12 +0900

    manage icon button focus style

--- app/components/ui/icon_button_component.rb
+++ app/components/ui/icon_button_component.rb
@@ -4,12 +4,19 @@ class Ui::IconButtonComponent < ViewComponent::Base
     @dropdown = attrs[:dropdown] || false
     @extra_data = attrs[:data] || {}
     @extra_class = attrs[:class] || []
+    @silent_focus = attrs[:silent_focus] || false
   end
 
   def data_for_dropdown
     return {} unless @dropdown
 
     { action: 'dropdown#toggle click@window->dropdown#hide' }
+  end
+
+  def focus_class
+    return %w(focus:ring-0) if @silent_focus
+
+    []
   end
 
   private
