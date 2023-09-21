b8a0b588988d3dd975ceee4d717a58d0ed4aebb4
hzms <h4zime@gmail.com>
2023-09-21 20:28:19 +0900

    no decorate option for item size

--- app/components/item_size_component.rb
+++ app/components/item_size_component.rb
@@ -1,6 +1,7 @@
 class ItemSizeComponent < ViewComponent::Base
-  def initialize(**args)
-    @size = args[:size]
+  def initialize(size:, decorate: true, **args)
+    @size = size
+    @decorate = decorate
     @element_size = args[:element_size] || :sm
     @extra_class = args[:class]
   end
