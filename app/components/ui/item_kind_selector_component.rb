2497e3895879a7fac99f9ba8e7a49e1c865ed15a
hzms <h4zime@gmail.com>
2023-09-21 20:15:30 +0900

    change to normal form control

--- app/components/ui/item_kind_selector_component.rb
+++
@@ -1,8 +0,0 @@
-class Ui::ItemKindSelectorComponent < ViewComponent::Base
-  def initialize(name:, kinds:, builder:, **options)
-    @name = name
-    @kinds = kinds
-    @builder = builder
-    @extra_class = options[:class] || []
-  end
-end
