864556d257d9f9e27e6193ecf1cadc31099f6430
hzms <h4zime@gmail.com>
2023-09-23 10:52:03 +0900

    add items#edit action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :kanban, only: [:show]
 
-  resources :items, only: [:new, :index, :show]
+  resources :items, only: [:index, :show, :new, :edit]
 
   resources :pages, only: [:index, :show]
 end
