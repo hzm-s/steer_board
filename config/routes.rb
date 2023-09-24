bab59893009947b356c731cfb0865df50205bea4
hzms <h4zime@gmail.com>
2023-09-24 10:51:10 +0900

    add new step action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resource :kanban, only: [:show]
-
+  resources :steps, only: [:new]
   resources :items, only: [:index, :show, :new, :edit]
 
   resources :pages, only: [:index, :show]
