517edc83ccd0683c3dedfdea6ecf001f9476dfe9
hzms <h4zime@gmail.com>
2023-09-30 10:34:35 +0900

    ./bin/importmap pin tailwindcss-stimulus-components --download

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "font-awesome", to: "https://kit.fontawesome.com/21f6899a03.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "sortablejs" # @1.15.0
+pin "tailwindcss-stimulus-components" # @4.0.4
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
