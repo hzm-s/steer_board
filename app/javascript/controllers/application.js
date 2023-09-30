f1127aa2d6c1314a411b770843f576e096a7373a
hzms <h4zime@gmail.com>
2023-09-30 20:09:30 +0900

    open modal when item forward

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,11 +2,13 @@ import { Application } from "@hotwired/stimulus"
 
 import Reveal from "stimulus-reveal-controller"
 import { Dropdown } from "tailwindcss-stimulus-components"
+import { Modal } from "tailwindcss-stimulus-components"
 
 const application = Application.start()
 
 application.register('reveal', Reveal)
 application.register('dropdown', Dropdown)
+application.register('modal', Modal)
 
 // Configure Stimulus development experience
 application.debug = false
