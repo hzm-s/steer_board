36835db345376c28abafc526f0581d2174b37d26
hzms <h4zime@gmail.com>
2023-09-30 10:58:47 +0900

    use stimulus-reveal-controller

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,11 @@
 import { Application } from "@hotwired/stimulus"
 
+import Reveal from "stimulus-reveal-controller"
 import { Dropdown } from "tailwindcss-stimulus-components"
 
 const application = Application.start()
 
+application.register('reveal', Reveal)
 application.register('dropdown', Dropdown)
 
 // Configure Stimulus development experience
