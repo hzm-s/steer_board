1b3418c88706b32d19edf622ba1b57476417e975
hzms <h4zime@gmail.com>
2023-10-07 10:26:24 +0900

    add route

---
+++ app/controllers/kanban/settings_controller.rb
@@ -0,0 +1,2 @@
+class Kanban::SettingsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resource :kanban, only: [:show]
+  resource :kanban, only: [:show] do
+    resource :setting, only: [:show], module: :kanban
+  end
+
   resources :steps, only: [:new]
   resources :items, only: [:index, :show, :new, :edit]
 
   resources :pages, only: [:index, :show]
+
+  root to: 'pages#index'
 end
