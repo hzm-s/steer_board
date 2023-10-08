2e8e0780c8f3d7078e0c36a66a8a1a98e402b996
hzms <h4zime@gmail.com>
2023-10-08 15:17:26 +0900

    item kind

--- app/models/item_kind.rb
+++ app/models/item_kind.rb
@@ -1,9 +1,29 @@
-class ItemKind < Struct.new(:name, keyword_init: true)
-  KINDS = %i(idea feature rework tech other).freeze
+class ItemKind < Struct.new(:name)
+  KINDS = %i(idea feature rework kaizen other).freeze
 
   class << self
     def all
-      KINDS.map { new(name: _1) }
+      KINDS.map { new(_1) }
     end
+
+    def from_name(name)
+      raise ArgumentError, "#{name} is invalid item kind" unless name.to_sym.in?(KINDS)
+
+      new(name.to_sym)
+    end
+
+    def load(name)
+      return from_name(name) if name
+
+      nil
+    end
+
+    def dump(obj)
+      obj.to_s
+    end
+  end
+
+  def to_s
+    name.to_s
   end
 end
