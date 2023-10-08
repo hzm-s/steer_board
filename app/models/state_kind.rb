c1860c08ec2094c8abfdbd345409421969b5021b
hzms <h4zime@gmail.com>
2023-10-08 13:01:05 +0900

    clean up

--- app/models/state_kind.rb
+++ app/models/state_kind.rb
@@ -1,10 +1,6 @@
 class StateKind
   module PreQueue
     class << self
-      def number
-        0
-      end
-
       def previous_state_of(step, work_flow)
         step.last_state_of_previous(work_flow)
       end
@@ -25,10 +21,6 @@ class StateKind
 
   module Primary
     class << self
-      def number
-        1
-      end
-
       def previous_state_of(step, work_flow)
         step.states.pre_queue || step.last_state_of_previous(work_flow)
       end
@@ -49,10 +41,6 @@ class StateKind
 
   module PostQueue
     class << self
-      def number
-        2
-      end
-
       def previous_state_of(step, work_flow)
         step.states.primary
       end
@@ -77,19 +65,5 @@ class StateKind
     def from_name(name)
       CLASSES[name]
     end
-
-    #def queue_from_position(pos)
-    #  from_name("#{pos}_queue".to_sym)
-    #end
-
-    #def load(number)
-    #  if number
-    #    CLASSES.values.find { _1.number == number }
-    #  end
-    #end
-
-    #def dump(obj)
-    #  obj.number
-    #end
   end
 end
