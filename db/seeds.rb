70344f0543fb4b425c5c7ef26be95d5ea540dbf5
hzms <h4zime@gmail.com>
2023-10-08 17:25:57 +0900

    seed item

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-if WorkFlow.count == 0 || ENV.fetch('FORCE', false)
+force = ENV.fetch('FORCE', false)
+
+if WorkFlow.count == 0 || force
   steps = [
     Step.new(name: 'Todo'),
     Step.new(name: 'Analyze'),
@@ -9,7 +11,67 @@ if WorkFlow.count == 0 || ENV.fetch('FORCE', false)
     Step.new(name: 'Production'),
   ]
   WorkFlow.new do |wf|
-    steps.each { wf.add_step(_1) }
+    steps.each do |step|
+      wf.add_step(step)
+    end
     wf.save!
+  end
+end
+
+if Item.count == 0 || force
+  wf = WorkFlow.last
+  todo, analyze, next10, dev, qa, deploy, prod = *wf.steps
+
+  items = {
+    todo.states.primary => [
+      Item.new(kind: ItemKind.idea, content: '[UI prototype] Cycle time'),
+      Item.new(kind: ItemKind.idea, content: '[UI prototype] Lead time'),
+      Item.new(kind: ItemKind.idea, content: '[UI prototype] Add item'),
+      Item.new(kind: ItemKind.idea, content: '[Dummy] ***'),
+      Item.new(kind: ItemKind.idea, content: '[Dummy] ***'),
+      Item.new(kind: ItemKind.idea, content: '[Dummy] ***'),
+      Item.new(kind: ItemKind.idea, content: '[Dummy] ***'),
+      Item.new(kind: ItemKind.idea, content: '[Dummy] ---'),
+    ],
+    analyze.states.primary => [
+      Item.new(kind: ItemKind.idea, content: '[UI prototype] Backlog'),
+    ],
+    next10.states.primary => [
+      Item.new(kind: ItemKind.feature, content: '[UI prototype] Item with tasks'),
+      Item.new(kind: ItemKind.feature, content: '[UI prototype] Move items'),
+      Item.new(kind: ItemKind.feature, content: '[UI prototype] Check to done'),
+    ],
+    dev.states.primary => [
+      Item.new(kind: ItemKind.kaizen, content: 'Board (use ViewComponent)'),
+      Item.new(kind: ItemKind.other, content: '[Dummy] ***'),
+      Item.new(kind: ItemKind.feature, content: '[Dummy] ***'),
+      Item.new(kind: ItemKind.feature, content: '[Dummy] ***'),
+      Item.new(kind: ItemKind.feature, content: '[Dummy] ***'),
+      Item.new(kind: ItemKind.feature, content: '[Dummy] ***'),
+      Item.new(kind: ItemKind.feature, content: '[Dummy] ***'),
+      Item.new(kind: ItemKind.feature, content: '[Dummy] ***'),
+      Item.new(kind: ItemKind.feature, content: '[Dummy] ---'),
+    ],
+    dev.states.post_queue => [
+      Item.new(kind: ItemKind.feature, content: '[UI prototype] Board buffer'),
+      Item.new(kind: ItemKind.feature, content: '[UI prototype] Board column'),
+    ],
+    qa.states.primary => [
+      Item.new(kind: ItemKind.feature, content: '[UI prototype] Index page'),
+      Item.new(kind: ItemKind.rework, content: '[UI prototype] Individual pages'),
+      Item.new(kind: ItemKind.other, content: '[Setup] DB'),
+      Item.new(kind: ItemKind.other, content: '[Setup] Repository'),
+    ],
+    prod.states.primary => [
+      Item.new(kind: ItemKind.other, content: '[Setup] git'),
+      Item.new(kind: ItemKind.other, content: '[Setup] rails'),
+    ],
+  }
+  items.each do |state, items_per_state|
+    items_per_state.each do |item|
+      item.initialize_status(wf)
+      item.change_status(state, wf)
+      item.save!
+    end
   end
 end
