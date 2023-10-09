194433ae3530cf01dacafe83df94f488075a2532
hzms <h4zime@gmail.com>
2023-10-09 18:42:57 +0900

    fix show step

--- app/components/ui/labeled_field_component.rb
+++ app/components/ui/labeled_field_component.rb
@@ -2,9 +2,10 @@ class Ui::LabeledFieldComponent < ViewComponent::Base
   renders_one :actions
   renders_one :custom_value
 
-  def initialize(label:, value: nil, size: :md, **options)
+  def initialize(label:, value: nil, size: :md, bg: true, **options)
     @label = label
     @value = value
     @size = size
+    @bg = bg
   end
 end
