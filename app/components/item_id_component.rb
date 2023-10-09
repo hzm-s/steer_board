a33d44261aec8ac090da305b73b0f7ca728e8a03
hzms <h4zime@gmail.com>
2023-10-09 11:34:39 +0900

    fix item id

--- app/components/item_id_component.rb
+++ app/components/item_id_component.rb
@@ -1,6 +1,6 @@
 class ItemIdComponent < ViewComponent::Base
-  def initialize(**args)
-    @id = args[:id]
-    @extra_class = args[:class] || []
+  def initialize(item:, **options)
+    @item = item
+    @extra_class = options[:class] || []
   end
 end
