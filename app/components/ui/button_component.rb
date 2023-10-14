12be0c2d200d03aae07814b883a03acb646be9eb
hzms <h4zime@gmail.com>
2023-10-14 12:04:38 +0900

    add href option to use anchor tag

--- app/components/ui/button_component.rb
+++ app/components/ui/button_component.rb
@@ -1,8 +1,9 @@
 class Ui::ButtonComponent < ViewComponent::Base
-  def initialize(kind: :primary, type: :submit, size: :md, form_builder: nil, **options)
+  def initialize(kind: :primary, type: :submit, size: :md, form_builder: nil, href: nil, **options)
     @kind = kind
     @type = type
     @size = size
     @form_builder = form_builder
+    @href = href
   end
 end
